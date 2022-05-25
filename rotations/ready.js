4ee2a989e6d2b7dac126cbcc24b84f1f46cfc00f
SDCored <sdcored@gmail.com>
2022-05-24 22:08:45 -0500

    Presence Update

--- rotations/ready.js
+++ rotations/ready.js
@@ -5,5 +5,5 @@ client.once('ready', client => {
 	console.log(`Logging in to ${client.user.username}...`);
 	console.log(`Logged in to ${client.user.username}!`);
 
-	client.user.setPresence({ activities: [{ name: 'the Apex Games', type: 'WATCHING' }] }, { status: 'online' });
+	client.user.setPresence({ activities: [{ name: `the Apex Games // v${version}`, type: 'WATCHING' }] }, { status: 'online' });
 });
