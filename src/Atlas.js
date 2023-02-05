261de3873cd65221faf9e09868a47a81d4efc9c6
SDCored <sdcored@gmail.com>
2023-02-05 02:55:07 -0600

    Basic Stuff

--- src/Atlas.js
+++ src/Atlas.js
@@ -1,1 +1,22 @@
-console.log('Ready!');
+const { Client, ActivityType, GatewayIntentBits, ApplicationCommandType } = require('discord.js');
+
+const config = require('./config.json');
+
+const { Guilds, GuildMessages } = GatewayIntentBits;
+
+const client = new Client({ intents: [Guilds, GuildMessages] });
+
+client.on('ready', async () => {
+	console.log(`${client.user.username} is Online.`);
+
+	client.user.setPresence({
+		activities: [
+			{
+				type: ActivityType.Watching,
+				name: `Apex Legends`,
+			},
+		],
+	});
+});
+
+client.login(config.discord.token);
