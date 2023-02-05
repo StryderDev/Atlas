61ae1803c3010c3b80c2e217b9cea2d2b6392bb2
SDCored <sdcored@gmail.com>
2023-02-05 02:34:23 -0600

    Begin Rework

--- rotations/ready.js
+++
@@ -1,9 +0,0 @@
-const { client } = require('../Rotation.js');
-const { version } = require('../package.json');
-
-client.once('ready', client => {
-	console.log(`Logging in to ${client.user.username}...`);
-	console.log(`Logged in to ${client.user.username}!`);
-
-	client.user.setPresence({ activities: [{ name: `the Apex Games // v${version}`, type: 'WATCHING' }] }, { status: 'online' });
-});
