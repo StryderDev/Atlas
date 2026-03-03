2b1a81aa39f4982b115819bb159bfa75b1f46945
SDCore <sdcored@gmail.com>
2026-03-03 17:39:13 -0600

    Bot Login

--- src/atlas.js
+++ src/atlas.js
@@ -1,1 +1,18 @@
-console.log('bon');
+const chalk = require('chalk');
+const { Client, GatewayIntentBits } = require('discord.js');
+
+Bun.env.TZ = 'America/Chicago';
+
+const client = new Client({
+	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildPresences, GatewayIntentBits.MessageContent],
+});
+
+client
+	.login(Bun.env.DISCORD_TOKEN)
+	.then(() => {
+		console.log(`${chalk.yellow.bold('[ATLAS_BOT]')} Logging in...`);
+		console.log(`${chalk.green.bold('[ATLAS_BOT]')} Discord Client Login Successful`);
+	})
+	.catch(err => {
+		console.log(`${chalk.red.bold('[ATLAS_BOT]')} Discord Client Gateway Error: ${err.message}`);
+	});
